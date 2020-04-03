3117cfdaf4e91c52d812ae59826790e2a86ac508
shvmsrma <shvmsrma@gmail.com>
2020-04-03 10:53:16 +0530

    Updates

---
+++ precache-manifest.1aff3a2ac2f8e9074cf2ffbbfa4286c4.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a52f97df671bbb8340e5d53cedfbbca1",
+    "url": "/MegaQuizApp/index.html"
+  },
+  {
+    "revision": "8b38a5e877d5d1d5921b",
+    "url": "/MegaQuizApp/static/css/2.8bd11672.chunk.css"
+  },
+  {
+    "revision": "f2dda90fdb1c1301570e",
+    "url": "/MegaQuizApp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8b38a5e877d5d1d5921b",
+    "url": "/MegaQuizApp/static/js/2.30221bc9.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/MegaQuizApp/static/js/2.30221bc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2dda90fdb1c1301570e",
+    "url": "/MegaQuizApp/static/js/main.1f99a7e2.chunk.js"
+  },
+  {
+    "revision": "6d6009b8575eb3c51b46",
+    "url": "/MegaQuizApp/static/js/runtime-main.477f44c0.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/MegaQuizApp/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/MegaQuizApp/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/MegaQuizApp/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/MegaQuizApp/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/MegaQuizApp/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/MegaQuizApp/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/MegaQuizApp/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/MegaQuizApp/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/MegaQuizApp/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/MegaQuizApp/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/MegaQuizApp/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/MegaQuizApp/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/MegaQuizApp/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/MegaQuizApp/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/MegaQuizApp/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/MegaQuizApp/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
