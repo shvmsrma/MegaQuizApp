fc86c7228ae179d3213bdeb4dbe06a2ba55bbe00
shvmsrma <shvmsrma@gmail.com>
2020-04-03 18:40:25 +0530

    Commit

--- src/components/questionslist.component.js
+++ src/components/questionslist.component.js
@@ -12,49 +12,22 @@ export default class QuestionList extends Component {
         super(props)
         this.handleOptionChange = this.handleOptionChange.bind(this);
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleIdChange =this.handleIdChange.bind(this);
-
-        this.state = {questions:[],
-          value:'',
-          id:''
-          
-          
-          }
+        this.state = {questions:[] }
         
     }
-    handleChange(e){
-      this.handleIdChange(e);
-      this.handleOptionChange(e);
-    }
     
-    handleOptionChange(e){
-      this.setState({ 
-        value:e.target.value 
-      }
-      )}
-    handleIdChange(e)
-    {
-      this.setState({
-        id:e.target.id
-      }
-    )}
+    handleOptionChange = (e, { value }) => this.setState({ value })
+
+    
+    
     
     handleFormSubmit (e) {
       e.preventDefault();
-    axios.get('https://megaquizappbackend.herokuapp.com/question/ '+ this.state.id)
-    .then(response => {
-      if(this.state.value ==response.ans){
-        console.log('Correct Answer')
-      }
-      else{
-        console.log('Wrong Answer')
-      }
-    })
-      console.log('You have selected:', this.state.value);
-      console.log('You have selected:', this.state.id);
+    console.log('You have selected:', this.state.value);
       
     }
+
+    
   
 
     componentDidMount(){
@@ -75,19 +48,17 @@ export default class QuestionList extends Component {
         return this.state.questions.map(currentquestion => {
 
           return(
-        <Form onSubmit ={this.handleFormSubmit}>
+        <Form onSubmit={this.handleFormSubmit}>
         <Form.Field>
          <h3>Q.  {currentquestion.ques}</h3>
-          <h6>Correct Answer is {currentquestion.ans} </h6>
         </Form.Field>
         <Form.Field>
           <Checkbox
             label={currentquestion.options[0]}
             name='checkboxRadioGroup'
-            id = {currentquestion._id}
             value ={currentquestion.options[0]}
-            checked = {this.state.value === currentquestion.options[0] && this.state.id === currentquestion.id}
-            onChange={this.handleChange}
+            checked = {this.state.value === currentquestion.options[0]}
+            onChange={this.handleOptionChange}
 
           />
         </Form.Field>
@@ -95,10 +66,9 @@ export default class QuestionList extends Component {
           <Checkbox
             label={currentquestion.options[1]}
             name='checkboxRadioGroup'
-            id = {currentquestion._id}
             value ={currentquestion.options[1]}
-            checked = {this.state.value === currentquestion.options[1] && this.state.id === currentquestion.id}
-            onChange={this.handleChange}
+            checked = {this.state.value === currentquestion.options[1]}
+            onChange={this.handleOptionChange}
 
           />
         </Form.Field>
@@ -106,10 +76,9 @@ export default class QuestionList extends Component {
           <Checkbox
             label={currentquestion.options[2]}
             name='checkboxRadioGroup'
-            id = {currentquestion._id}
             value = {currentquestion.options[2]}
-            checked = {this.state.value === currentquestion.options[2] && this.state.id === currentquestion._id}
-            onChange={this.handleChange}
+            checked = {this.state.value === currentquestion.options[2]}
+            onChange={this.handleOptionChange}
 
           />
         </Form.Field>
@@ -117,10 +86,9 @@ export default class QuestionList extends Component {
           <Checkbox
             label={currentquestion.options[3]}
             name='checkboxRadioGroup'
-            id = {currentquestion._id}
             value={currentquestion.options[3]}
-            checked = {this.state.value ===currentquestion.options[3] && this.state.id === currentquestion._id}
-            onChange={this.handleChange}
+            checked = {this.state.value ===currentquestion.options[3]}
+            onChange={this.handleOptionChange}
 
           />
         </Form.Field>
---
+++ src/components/resultcomp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class ResultComp extends Component {
+
+    render(){
+        return(
+            <p>Correct</p>
+        )
+    }
+
+
+}
