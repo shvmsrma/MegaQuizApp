e846dc2a318a96a8898bdad4b2d32767ddab5190
shvmsrma <shvmsrma@gmail.com>
2020-04-03 16:28:48 +0530

    s

--- src/components/questionslist.component.js
+++ src/components/questionslist.component.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios'
-import {Form,Radio,} from 'semantic-ui-react'
+import {Form,Checkbox} from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css';
 
 
@@ -12,22 +12,48 @@ export default class QuestionList extends Component {
         super(props)
         this.handleOptionChange = this.handleOptionChange.bind(this);
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
-    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleIdChange =this.handleIdChange.bind(this);
+
         this.state = {questions:[],
-          answer:''}
+          value:'',
+          id:''
+          
+          
+          }
         
+    }
+    handleChange(e){
+      this.handleIdChange(e);
+      this.handleOptionChange(e);
     }
     
     handleOptionChange(e){
+      this.setState({ 
+        value:e.target.value 
+      }
+      )}
+    handleIdChange(e)
+    {
       this.setState({
-        answer: e.target.value
-      });
-    }
+        id:e.target.id
+      }
+    )}
     
     handleFormSubmit (e) {
       e.preventDefault();
-    
-      console.log('You have selected:', this.state.answer);
+    axios.get('https://megaquizappbackend.herokuapp.com/question/ '+ this.state.id)
+    .then(response => {
+      if(this.state.value ==response.ans){
+        console.log('Correct Answer')
+      }
+      else{
+        console.log('Wrong Answer')
+      }
+    })
+      console.log('You have selected:', this.state.value);
+      console.log('You have selected:', this.state.id);
+      
     }
   
 
@@ -49,48 +75,52 @@ export default class QuestionList extends Component {
         return this.state.questions.map(currentquestion => {
 
           return(
-            <Form onSubmit ={this.handleFormSubmit}>
+        <Form onSubmit ={this.handleFormSubmit}>
         <Form.Field>
          <h3>Q.  {currentquestion.ques}</h3>
           <h6>Correct Answer is {currentquestion.ans} </h6>
         </Form.Field>
         <Form.Field>
-          <Radio
+          <Checkbox
             label={currentquestion.options[0]}
-            name='radioGroup'
+            name='checkboxRadioGroup'
+            id = {currentquestion._id}
             value ={currentquestion.options[0]}
-            checked = {this.state.answer === currentquestion.options[0]}
-            onChange={this.handleOptionChange}
+            checked = {this.state.value === currentquestion.options[0] && this.state.id === currentquestion.id}
+            onChange={this.handleChange}
 
           />
         </Form.Field>
         <Form.Field>
-          <Radio
+          <Checkbox
             label={currentquestion.options[1]}
-            name='radioGroup'
+            name='checkboxRadioGroup'
+            id = {currentquestion._id}
             value ={currentquestion.options[1]}
-            checked = {this.state.answer === currentquestion.options[1]}
-            onChange={this.handleOptionChange}
+            checked = {this.state.value === currentquestion.options[1] && this.state.id === currentquestion.id}
+            onChange={this.handleChange}
 
           />
         </Form.Field>
         <Form.Field>
-          <Radio
+          <Checkbox
             label={currentquestion.options[2]}
-            name='radioGroup'
+            name='checkboxRadioGroup'
+            id = {currentquestion._id}
             value = {currentquestion.options[2]}
-            checked = {this.state.answer === currentquestion.options[2]}
-            onChange={this.handleOptionChange}
+            checked = {this.state.value === currentquestion.options[2] && this.state.id === currentquestion._id}
+            onChange={this.handleChange}
 
           />
         </Form.Field>
         <Form.Field>
-          <Radio
+          <Checkbox
             label={currentquestion.options[3]}
-            name='radioGroup'
+            name='checkboxRadioGroup'
+            id = {currentquestion._id}
             value={currentquestion.options[3]}
-            checked = {this.state.answer === currentquestion.options[3]}
-            onChange={this.handleOptionChange}
+            checked = {this.state.value ===currentquestion.options[3] && this.state.id === currentquestion._id}
+            onChange={this.handleChange}
 
           />
         </Form.Field>
